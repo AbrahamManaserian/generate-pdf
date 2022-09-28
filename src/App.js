6a2d971c2cf0d6ecfed4660904357d33afb96eb4
AbrahamManaserian <abraham020788@gmail.com>
2022-09-28 15:33:41 +0400

    setup d3.js-to-react file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 // import GeneratePdf from './components/GeneratePdf';
 import CreateCV from './containerPages/CreateCV'
 import HomePage from './containerPages/HomePage'
+import D3ToReact from './components/D3ToReact'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<HomePage/>}/>
+        <Route path='/d3.js-to-react' element={<D3ToReact/>}/>
         <Route path='/createCV' element={<CreateCV/>}>
           <Route path=':template' element={<CreateCV/>}/>
         </Route>
