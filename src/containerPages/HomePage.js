ec0c197c48ddfcac482befce050b3f5ed38d7dc6
AbrahamManaserian <abraham020788@gmail.com>
2024-03-08 17:50:31 +0400

    change grid

--- src/containerPages/HomePage.js
+++ src/containerPages/HomePage.js
@@ -15,27 +15,25 @@ export default function HomePage () {
     }
     writeData()
     return (
-        <Grid item container xs ={12} spacing={3}>
-            <Grid item container xs={12} justifyContent='center' sx={{marginTop:3}}>
-                <Typography>
-                    Choose the template
-                </Typography>
-            </Grid>
-            <Grid item container xs={4}>
-              <Link to='/createCV/template1'>
-                <img src={image1} style={{width:'100%',height:'auto'}}/>
-              </Link>
-            </Grid>
-            <Grid item container xs={4}>
-              <Link to='/createCV/template2' >
-                <img src={image2} style={{width:'100%',height:'100%'}}/>
-              </Link>
-            </Grid>
-            <Grid item container xs={4}>
-              <Link to='/createCV/template3'>
-                 <img src={image3} style={{width:'100%',height:'auto'}}/>
-              </Link>
-            </Grid>
+      <Grid item container xs={12} spacing={3}>
+        <Grid item container xs={12} justifyContent="center" sx={{ marginTop: 3 }}>
+          <Typography>Choose the template</Typography>
         </Grid>
-    )
+        <Grid item container xs={6} sm={4}>
+          <Link to="/createCV/template1">
+            <img src={image1} style={{ width: '100%', height: 'auto' }} />
+          </Link>
+        </Grid>
+        <Grid item container xs={6} sm={4}>
+          <Link to="/createCV/template2">
+            <img src={image2} style={{ width: '100%', height: '100%' }} />
+          </Link>
+        </Grid>
+        <Grid item container xs={12} sm={4}>
+          <Link to="/createCV/template3">
+            <img src={image3} style={{ width: '100%', height: 'auto' }} />
+          </Link>
+        </Grid>
+      </Grid>
+    );
 }
